607ae3bc015d522ffc46d5f61ebe064ce2b97a22
yuyan <yuyan_zhao@outlook.com>
2024-11-22 12:03:21 +0100

    Add search repositories

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ import requests
 from utils import save_json
 
 
-def get_repositories():
-    url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-    headers = {'Accept': 'application/vnd.github.v3+json'}
-    response = requests.get(url, headers=headers)
-    print(f"Status code: {response.status_code}")
+
+def get_repositories(params):
+    url = 'https://api.github.com/search/repositories'
+    headers = {'Accept': 'application/vnd.github+json'}
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code != 200:
+        raise Exception(f"Error: status code {response.status_code}")
+
     response_json = response.json()
     return response_json
 
 
 if __name__ == "__main__":
-    data = get_repositories()
-    print(f"Total repositories: {data['total_count']}")
-    save_json(data, "data/repositories_python.json")
+    try:
+        params = {
+            "q": "is:public stars:>=100",
+            "sort": "stars"
+        }
+        repositories = get_repositories(params)
+        print(f"Total repositories: {repositories['total_count']}")
+        save_json(repositories, "data/repositories.json")
+    except Exception as e:
+        print(e)
+
